64e1a70d8ff64d00991c002d2a7cc6b411612175
julia-nichole <52431854+julia-nichole@users.noreply.github.com>
2019-11-04 09:17:39 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,144 +1,151 @@
 
-    const player = {
-        '1': 'pink',
-        '-1': 'purple',
-        'null': 'white'
-      };
-      
-      let winCom = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
-      
-      let board, turn, winner
-      
-      
-      let squares = document.querySelectorAll('td div');
-      let  message = document.querySelector('h1');
-      
-      
-      document.querySelector('table').addEventListener('click', handleMove);
-      document.querySelector('button').addEventListener('click', initialize);
-      
-    
-      
-      initialize();
-      
-      function handleMove(evt) {
-       
-        let idx = parseInt(evt.target.id.replace('sq', ''));
-        
-        if (board[idx] || winner) return;
-        
-        board[idx] = turn;
-        turn *= -1;
-        winner = getWinner();
-        render();
-      }
-      
-      function getWinner() {
-        for (let i = 0; i < winCom.length; i++) {
-          if (Math.abs(board[winCom[i][0]] + board[winCom[i][1]] + board[winCom[i][2]]) === 3) return board[winCom[i][0]];
-        }
-       
-        if (board.includes(null)) return null;
-        return 'T';
-      }
-      
-      function render() {
-        board.forEach(function(sq, idx) {
-          squares[idx].style.background = player[sq];
-        });
-        if (winner === 'T') {
-          message.innerHTML = 'TIE';
-        } else if (winner) {
-          message.innerHTML = `CONGRATS  ${player[winner].toUpperCase()}!`;
-        } else {
-          message.innerHTML = `${player[turn]}'s Turn`;
-        }
-      }
-      
-      function initialize() {
-      board = new Array(9).fill(null);
-     turn = 1;
-     winner = null;
-     render();
-       }
+
+ const player = {
+  '1': 'pink',
+  '-1': 'purple',
+  'null': 'white'
+};
+
+let winCom = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let board, turn, winner
+
+
+let squares = document.querySelectorAll('td div');
+let  message = document.querySelector('h1');
+
+
+document.querySelector('table').addEventListener('click', handleMove);
+document.querySelector('button').addEventListener('click', initialize);
 
 
 
-//       // const 
-//       const sprite = {
-//           "1" : "X",
-//           "-1" : "O",
-//           "null" : ''
-//       };
-//       const combo = [
+initialize();
 
-//        [0,1,2],
-//        [3,4,5],
-//        [6,7,8],
-//        [6,7,8],
-//        [2,5,8],
-//        [0,4,8],
-//        [2,4,6],
+function handleMove(evt) {
+ 
+  let idx = parseInt(evt.target.id.replace('sq', ''));
+  
+  if (board[idx] || winner) return;
+  
+  board[idx] = turn;
+  turn *= -1;
+  winner = getWinner();
+  render();
+}
 
+function getWinner() {
+  for (let i = 0; i < winCom.length; i++) {
+    if (Math.abs(board[winCom[i][0]] + board[winCom[i][1]] + board[winCom[i][2]]) === 3) return board[winCom[i][0]];
+  }
+ 
+  if (board.includes(null)) return null;
+  return 'T';
+}
 
-//       ];
-//       let board,turn,winner;
+function render() {
+  board.forEach(function(sq, idx) {
+    squares[idx].style.background = player[sq];
+  });
+  if (winner === 'T') {
+    message.innerHTML = 'TIE';
+  } else if (winner) {
+    message.innerHTML = `CONGRATS  ${player[winner].toUpperCase()}!`;
+  } else {
+    message.innerHTML = `${player[turn]}'s Turn`;
+  }
+}
 
-//       const message = document.querySelector(".h2")
-//       const squares = document.querySelectorAll(".squares");
+function initialize() {
+board = new Array(9).fill(null);
+turn = 1;
+winner = null;
+render();
+ }
 
-//  document.querySelector('button').addEventListener("click",init);
-//  document.querySelector("game-board").addEventListener("click",handleMove);
-//  function init (){
-//      board = new Array(9).fill(null);
-//      turn = 1;
-//      winner = null;
-//      render();
-//  }
-//  function handleMove(evt){
-//    const moveIdx = parseInt(evt.target.dataset.square);
-//    board[moveIdx] = turn;
-//    turn *= -1;
-//    winner = getWinner();
-//      console.log(evt);
+// /*----- constants -----*/ 
+// const COLORS = { // define color object for player moves
+//   'null': '',
+//   '1': 'white',
+//   '-1': 'blue' 
+// };
 
-//  }
+// const winCombos = [
 
-//  function render(){
-//      board.forEach(function(elem,Idx){
-//          squares[idx].textContent = sprite[elem];
-     
-         
-//      });
-//       getWinner(){
-//         for(let i = 0, < combos.length){
-//             if(math.abs(board[combos[i][0]] +
-//                        (board[combos[i][1]] + 
-//                        (board[combos[i][2]]) ===3) return board[combos[i][0]];
-//         }
-//          if(board.includes(null)) return null;
-//          return "T"
-// //       }
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ];
 
-// //      if(!winner){
-// //          message.textContent = "${sprite}'s turn "
-// //      } else if (winner === "T"){
-//          message.textContent " tie ";
+// /*----- app's state (variables) -----*/ 
+// let board, turn, winner; 
 
+// /*----- cached element references -----*/ 
+// const message = document.querySelector('h1');
+// const squares = document.querySelectorAll('div')
+
+// /*----- event listeners -----*/ 
+// document.querySelector('button').addEventListener('click', init);
+// document.querySelector('section').addEventListener('click', handleClick);
+
+// /*----- functions -----*/
+// init();
+
+// function init() {
+
+//   board = new Array(9).fill(null);
+//   turn = 1;
+//   winner = null;
+//   render();
 // }
 
+// function handleClick(evt) {
+ 
+//   let moveIdx = parseInt(evt.target.dataset.square);
+//   if(board[moveIdx] || winner) return;
+  
+//   board[moveIdx] = turn;
+//   turn *= -1;
+//   winner = getWinner();
+//  render();
+// }
 
- // board = [null, null, null, null, null, null, null, null, null];
-    //     // board = new Array(9).fill(null);
-    //     turn = 1;
-    //     winner = null;
-    //     render();
+// function getWinner() {
+//   for(let i = 0; i <winCombos.length; i++){
+//       if(Math.abs(board[winCombos[i][0]] + 
+//                   board[winCombos[i][1]] + 
+//                   board[winCombos[i][2]] === 3)) return board[winCombos[i][0]];
+//   }
+//   if(board.includes(null)) return null;
+//   return "T";
+// }
+
+// function render() {
+  
+//   board.forEach(function(elem, idx) {
+      
+//       squares[idx].textContent = COLORS[elem];
+//   });
+//   if(!winner) {
+//       message.textContent = `${COLORS[turn]}'s Turn`;
+//   } else if(winner === "T") {
+//       message.textContent = 'Tie Game!';
+//   } else {
+//       message.textContent = `${COLORS[winner]} Wins!`;
+
+//   }
+//   }
